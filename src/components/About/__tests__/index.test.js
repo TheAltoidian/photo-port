63ea7e917f8c3e815d20e0264347de89405acce9
Cullen <cullenjcasey@gmail.com>
2022-05-25 16:23:37 -0700

    added photo list

--- src/components/About/__tests__/index.test.js
+++ src/components/About/__tests__/index.test.js
@@ -1,18 +1,19 @@
-import REACT from 'react';
-import { render, cleanup } from '@testing-library/react'
+// __tests__/About.test.js
+import React from 'react';
+import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import About from '..';
 
 afterEach(cleanup);
 
-describe('About component', () => {
+describe('About component renders', () => {
+  it('renders', () => {
+    render(<About />);
+  });
+  
+  it('matches snapshot DOM node structure', () => {
+    const { asFragment } = render(<About />);
     
-    it('renders', () => {
-        render(<About />);
-    });
-
-    it('matches snapshot DOM node structure', () => {
-        const { asFragment } = render (<About />);
-        expect(asFragment()).toMatchSnapshot();
-    })
+    expect(asFragment()).toMatchSnapshot();
+  });
 })
